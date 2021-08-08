d5d10de3761479c281b0801bbd61eb432f9c3a45
lovila <88611345+lovila@users.noreply.github.com>
2021-08-08 15:23:27 +0700

    Update questions.sql

--- SQL Deep Dive/Operator precendence/questions.sql
+++ SQL Deep Dive/Operator precendence/questions.sql
@@ -7,6 +7,9 @@
 * Include people that are 50
 * that are from either Japan or Australia
 */
+select * from Customers
+where (age < 30 or age > 50) and income > 50000 and (country = Japan or country = Australia);
+
 
 /*
 * DB: Store
@@ -14,3 +17,6 @@
 * Question: 
 * What was our total sales in June of 2004 for orders over 100 dollars?
 */
+
+select count orderid from orders
+where (orderdate >= 2004-06-01 and orderdate <= 2004-06-30) and totalamount > 100;
